a73c498fb1bdb05d10d87acca63d4e5ed26aede4
Hadrian Hughes <h.hughes@amaze.com>
2018-09-08 19:10:54 +0100

    Create sample component using styled-components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
+import Example from './components/Example';
+
+ReactDOM.render(<Example />, document.getElementById('root'));
