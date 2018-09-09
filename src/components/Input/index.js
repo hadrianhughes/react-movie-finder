b5ec7cf6dccf693b27ae7620ff53248b98806393
Hadrian Hughes <h.hughes@amaze.com>
2018-09-09 01:07:02 +0100

    Bind value of search input to value in container state

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,20 +3,27 @@ import PropTypes from 'prop-types';
 
 import { Wrapper, StyledInput } from './styles';
 
-const Input = ({ onChange, placeholder }) => (
+const Input = ({ onChange, placeholder, value }) => (
   <Wrapper>
-    <StyledInput type="text" onChange={onChange} placeholder={placeholder} />
+    <StyledInput
+      type="text"
+      onChange={onChange}
+      placeholder={placeholder}
+      value={value}
+    />
   </Wrapper>
 );
 
 Input.propTypes = {
   onChange: PropTypes.func,
   placeholder: PropTypes.string,
+  value: PropTypes.string,
 };
 
 Input.defaultProps = {
   onChange: () => {},
   placeholder: '',
+  value: '',
 };
 
 export default Input;
